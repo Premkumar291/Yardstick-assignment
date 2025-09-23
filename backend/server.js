726a2ff1eeea9ceb142cca3f1fcb0b117f7e34cb
Premkumar291 <premkumar.29105@gmail.com>
2025-09-23 08:13:30 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Basic route
+app.get('/', (req, res) => {
+    res.json({ message: 'Backend server is running!' });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server Started and Running on http://localhost:${PORT}`);
+});
